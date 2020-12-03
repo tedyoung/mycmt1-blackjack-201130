c3b5b5fa4f5f854e85f9245fb4c96b8dbffc4f54
Ted M. Young <tedyoung@gmail.com>
2020-12-03 00:04:57 -0800

    Lab 6: Found and fixed border case for Ace handling in determining hand value.

--- src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
@@ -13,7 +13,7 @@ public class HandValueAceTest {
   @Test
   public void handWithOneAceTwoCardsIsValuedAt11() throws Exception {
     var cards = List.of(new Card(DUMMY_SUIT, "A"),
-                       new Card(DUMMY_SUIT, "5"));
+                        new Card(DUMMY_SUIT, "5"));
     Hand hand = new Hand(cards);
 
     assertThat(hand.valueEquals(11 + 5))
@@ -24,13 +24,26 @@ public class HandValueAceTest {
   @Test
   public void handWithOneAceAndOtherCardsEqualTo11IsValuedAt1() throws Exception {
     var cards = List.of(new Card(DUMMY_SUIT, "A"),
-                       new Card(DUMMY_SUIT, "8"),
-                       new Card(DUMMY_SUIT, "3"));
+                        new Card(DUMMY_SUIT, "8"),
+                        new Card(DUMMY_SUIT, "3"));
     Hand hand = new Hand(cards);
 
     assertThat(hand.valueEquals(1 + 8 + 3))
         .describedAs("Value was " + hand.displayValue())
         .isTrue();
   }
+
+
+  @Test
+  public void handWithOneAceAndTenCardIsTotalValueOf21() throws Exception {
+    var cards = List.of(new Card(DUMMY_SUIT, "A"),
+                        new Card(DUMMY_SUIT, "Q"));
+    Hand hand = new Hand(cards);
+
+    assertThat(hand.valueEquals(11 + 10))
+        .describedAs("Value was " + hand.displayValue())
+        .isTrue();
+  }
+
 
 }
