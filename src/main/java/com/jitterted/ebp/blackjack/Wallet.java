bea1cffa91d7f0dafea827d98322800edc1da474
Ted M. Young <tedyoung@gmail.com>
2020-12-03 00:55:15 -0800

    Implemented baseline Wallet code via TDD for isEmpty, balance, and addMoney.

---
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -0,0 +1,21 @@
+package com.jitterted.ebp.blackjack;
+
+public class Wallet {
+
+  private int balance = 0;
+
+  public boolean isEmpty() {
+    return balance == 0;
+  }
+
+  public void addMoney(int amount) {
+    if (amount < 0) {
+      throw new IllegalArgumentException();
+    }
+    balance += amount;
+  }
+
+  public int balance() {
+    return balance;
+  }
+}
